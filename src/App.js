691f04c6de6f0a3c3a1fcb02ac541c3e42ed735f
ishcenko <a_ishcenko@i.ua>
2024-02-22 10:01:07 +0200

    Counter components

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Counter from './components/Counter/Counter';
+
+const App = () => (
+  <>
+    <h1>Стан компонента</h1>
+    <Counter />
+  </>
+);
+export default App;
